deaefa344bc7faf19e305a6de2780cad7fc79279
magomedgadjiev <magomedalievic@gmail.com>
2016-12-12 17:02:55 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,79 +1,84 @@
-#include <sort.hpp>
+#include "ext_sort.cpp"
 #include <catch.hpp>
-#include <string>
 #include <iostream>
+#include <string>
 #include <fstream>
-
-// SCENARIO("8mb", "[8mb]")
-// {
-//   REQUIRE(1 == 1);
-// }
-
-SCENARIO("8mb", "[8mb]")
-{
-  File_sort other("8mb", "out_8", 1);
-  bool x = true;
-  std::ifstream f1("out_8"), f2("out8");
-  Data s1, s2;
-  while(!f2.eof() && !f1.eof())
-  {
-    if((f1>>s1)&&(f2>>s2))
-    {
-      if(s1.name!=s2.name)
-      {
-        x=false;
-        break;
-      }
-    }
-    else break;
+#include <locale> 
+using namespace std;
+ 
+SCENARIO("8mb", "[8mb]"){
+ setlocale(LC_ALL, "Russian");
+ sorting("8.txt","8out_name.txt",1);
+ ifstream file("test8_name.txt");
+ifstream hay("test8_name.txt");
+int i=0;
+ bool p=true;
+ string s1,s2;
+ while (!hay.eof()&&!file.eof()){
+ getline(file,s1);
+ getline(hay,s2);
+  i++;
+  if (s1!=s2){
+  p=false;
+   cout<<i<<endl;
+   cout<<s1<<endl;
+   cout<<s2<<endl;
+   break;
   }
-  f1.close();
-  f2.close();
-  REQUIRE(x);
+ }
+  file.close();
+  hay.close();
+  REQUIRE(p==true);
 }
 
-// SCENARIO("15mb", "[15mb]")
-// {
-//   File_sort other("15mb", "out_15", 4);
-//   bool x = true;
-//   std::ifstream f1("out_15"), f2("out15");
-//   Data s1, s2;
-//   while(!f2.eof() && !f1.eof())
-//   {
-//     if((f1>>s1)&&(f2>>s2))
-//     {
-//       if(s1.name!=s2.name)
-//       {
-//         x=false;
-//         break;
-//       }
-//     }
-//     else break;
-//   }
-//   f1.close();
-//   f2.close();
-//   REQUIRE(x);
-// }
 
-// SCENARIO("32mb", "[32mb]")
-// {
-//   File_sort other("32mb", "out_32", 17);
-//   bool x = true;
-//   std::ifstream f1("out_32"), f2("out32");
-//   Data s1, s2;
-//   while(!f2.eof() && !f1.eof())
-//   {
-//     if((f1>>s1)&&(f2>>s2))
-//     {
-//       if(s1.name!=s2.name)
-//       {
-//         x=false;
-//         break;
-//       }
-//     }
-//     else break;
-//   }
-//   f1.close();
-//   f2.close();
-//   REQUIRE(x);
-// }
+SCENARIO("15mb", "[15mb]"){
+ setlocale(LC_ALL, "Russian");
+ sorting("15.txt","15out_name.txt",4);
+  ifstream hay("test15_name.txt");
+ifstream file("test15_name.txt");
+int i=0;
+ bool p=true;
+ string s1,s2;
+ while (!hay.eof()&&!file.eof()){
+getline(file,s1);
+ getline(hay,s2);
+  i++;
+  if (s1!=s2){
+  p=false;
+   cout<<i<<endl;
+   cout<<s1<<endl;
+   cout<<s2<<endl;
+   break;
+  }
+ }
+  file.close();
+ hay.close();
+  REQUIRE(p==true);
+}
+
+SCENARIO("32mb", "[32mb]"){
+ setlocale(LC_ALL, "Russian");
+ sorting("32.txt","32out_name.txt",17);
+ ifstream hay("test32_name.txt");
+ifstream file("test32_name.txt");
+int i=0;
+ bool p=true;
+ string s1,s2;
+ while (!hay.eof()&&!file.eof()){
+getline(file,s1);
+ getline(hay,s2);
+  i++;
+  if (s1!=s2){
+  p=false;
+   cout<<i<<endl;
+   cout<<s1<<endl;
+   cout<<s2<<endl;
+   break;
+  }
+ }
+  file.close();
+ hay.close();
+  REQUIRE(p==true);
+}
+
