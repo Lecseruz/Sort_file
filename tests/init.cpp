3a87bc40ba0fc2890778fcd45b38d669b1659ff0
magomedgadjiev <magomedalievic@gmail.com>
2016-12-12 17:04:23 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -33,48 +33,50 @@ SCENARIO("8mb", "[8mb]")
   REQUIRE(x);
 }
 
-// SCENARIO("15mb", "[15mb]")
-// {
-//   File_sort other("15mb", "out_15", 4);
-//   bool x = true;
-//   std::ifstream f1("out_15"), f2("out15");
-//   Data s1, s2;
-//   while(!f2.eof() && !f1.eof())
-//   {
-//     if((f1>>s1)&&(f2>>s2))
-//     {
-//       if(s1.name!=s2.name)
-//       {
-//         x=false;
-//         break;
-//       }
-//     }
-//     else break;
-//   }
-//   f1.close();
-//   f2.close();
-//   REQUIRE(x);
-// }
+SCENARIO("15mb", "[15mb]")
+{
+  setlocale(LC_ALL, "Russian");
+  sorting("15mb", "out_15", 4);
+  bool x = true;
+  std::ifstream f1("out_15"), f2("out15");
+  Data s1, s2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>s1)&&(f2>>s2))
+    {
+      if(s1.name!=s2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
+}
 
-// SCENARIO("32mb", "[32mb]")
-// {
-//   File_sort other("32mb", "out_32", 17);
-//   bool x = true;
-//   std::ifstream f1("out_32"), f2("out32");
-//   Data s1, s2;
-//   while(!f2.eof() && !f1.eof())
-//   {
-//     if((f1>>s1)&&(f2>>s2))
-//     {
-//       if(s1.name!=s2.name)
-//       {
-//         x=false;
-//         break;
-//       }
-//     }
-//     else break;
-//   }
-//   f1.close();
-//   f2.close();
-//   REQUIRE(x);
-// }
+SCENARIO("32mb", "[32mb]")
+{
+  setlocale(LC_ALL, "Russian");
+  sorting("32mb", "out_32", 17);
+  bool x = true;
+  std::ifstream f1("out_32"), f2("out32");
+  line s1, s2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>s1)&&(f2>>s2))
+    {
+      if(s1.name!=s2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
+}
