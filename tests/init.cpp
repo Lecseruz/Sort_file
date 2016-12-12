371df7d42c93e58684b1908d2aace5086260c299
magomedgadjiev <magomedalievic@gmail.com>
2016-12-12 16:32:49 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -32,53 +32,53 @@ int i=0;
 }
 
 
-SCENARIO("15mb", "[15mb]"){
- setlocale(LC_ALL, "Russian");
- sorting("15mb","15out_name.txt",4);
-  ifstream hay("test15_name.txt");
-ifstream file("test15_name.txt");
-int i=0;
- bool p=true;
- string s1,s2;
- while (!hay.eof()&&!file.eof()){
-getline(file,s1);
- getline(hay,s2);
-  i++;
-  if (s1!=s2){
-  p=false;
-   cout<<i<<endl;
-   cout<<s1<<endl;
-   cout<<s2<<endl;
-   break;
-  }
- }
-  file.close();
- hay.close();
-  REQUIRE(p==true);
-}
+// SCENARIO("15mb", "[15mb]"){
+//  setlocale(LC_ALL, "Russian");
+//  sorting("15mb","15out_name.txt",4);
+//   ifstream hay("test15_name.txt");
+// ifstream file("test15_name.txt");
+// int i=0;
+//  bool p=true;
+//  string s1,s2;
+//  while (!hay.eof()&&!file.eof()){
+// getline(file,s1);
+//  getline(hay,s2);
+//   i++;
+//   if (s1!=s2){
+//   p=false;
+//    cout<<i<<endl;
+//    cout<<s1<<endl;
+//    cout<<s2<<endl;
+//    break;
+//   }
+//  }
+//   file.close();
+//  hay.close();
+//   REQUIRE(p==true);
+// }
 
-SCENARIO("32mb", "[32mb]"){
- setlocale(LC_ALL, "Russian");
- sorting("32mb","32out_name.txt",17);
- ifstream hay("test32_name.txt");
-ifstream file("test32_name.txt");
-int i=0;
- bool p=true;
- string s1,s2;
- while (!hay.eof()&&!file.eof()){
-getline(file,s1);
- getline(hay,s2);
-  i++;
-  if (s1!=s2){
-  p=false;
-   cout<<i<<endl;
-   cout<<s1<<endl;
-   cout<<s2<<endl;
-   break;
-  }
- }
-  file.close();
- hay.close();
-  REQUIRE(p==true);
-}
+// SCENARIO("32mb", "[32mb]"){
+//  setlocale(LC_ALL, "Russian");
+//  sorting("32mb","32out_name.txt",17);
+//  ifstream hay("test32_name.txt");
+// ifstream file("test32_name.txt");
+// int i=0;
+//  bool p=true;
+//  string s1,s2;
+//  while (!hay.eof()&&!file.eof()){
+// getline(file,s1);
+//  getline(hay,s2);
+//   i++;
+//   if (s1!=s2){
+//   p=false;
+//    cout<<i<<endl;
+//    cout<<s1<<endl;
+//    cout<<s2<<endl;
+//    break;
+//   }
+//  }
+//   file.close();
+//  hay.close();
+//   REQUIRE(p==true);
+// }
 
